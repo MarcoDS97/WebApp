f38307a2afc199fc93fc43d776609a4386df2d67
ale-comi <alecomi95@gmail.com>
2024-02-08 17:05:01 +0100

    prova push di modifiche script csv

--- modifica_csv.py
+++ modifica_csv.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 import csv
-from faker import Faker
+import random
 
 
 file = open("Giocatori.csv", mode="r", encoding="utf-8", newline=""):
@@ -12,9 +12,18 @@ scrittore = csv.reader(nuovo_file, delimiter=";")
 
 next(lettore)
 
+# N. maglia --> creare set con 99 numeri e fare .pop()
+# Età --> random number in range 18-35
+# ruolo --> lista con 4 elementi, dove il portiere è 1 random range(2-12) //3 per decidere gli altri ruoli 10% portiere, 30% altri ruoli
+
+
+
+
 for riga in lettore:
     print(riga)
 
 nuovo_file.close()
 file.close()
+
+
 
