99a29407c99ab220049ad32edc2601f402b693dd
MarcoDS97 <mdesio97@gmail.com>
2024-02-08 16:14:20 +0100

    Aggiunto frontend

---
+++ route.py
@@ -0,0 +1,12 @@
+import json
+import mysql.connector
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
