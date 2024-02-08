21d570127ccf5594f3db3226c69a76be6432a1d6
ale-comi <alecomi95@gmail.com>
2024-02-08 16:18:44 +0100

    modifiche script per csv

--- modifica_csv.py
+++ modifica_csv.py
@@ -5,12 +5,15 @@ from faker import Faker
 
 
 file = open("Giocatori.csv", mode="r", encoding="utf-8", newline=""):
-    nuovo_file = open("Giocatori_nuovo.csv", )
-    lettore = csv.reader(file, delimiter=";")
-    next(lettore)
+nuovo_file = open("Giocatori_nuovo.csv", mode="x", encoding="utf-8", newline="")
 
-    for riga in lettore:
-        print(riga)
+lettore = csv.reader(file, delimiter=";")
+scrittore = csv.reader(nuovo_file, delimiter=";")
+
+next(lettore)
+
+for riga in lettore:
+    print(riga)
 
 nuovo_file.close()
 file.close()
