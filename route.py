e4b24d2f22dc6d7d4cd541e1fdb5e0530f7033be
Zerok00 <lucarellidavide5@gmail.com>
2024-02-09 11:08:55 +0100

    Aggiunti file html e route

--- route.py
+++ route.py
@@ -8,9 +8,13 @@ app = Flask(__name__)
 def homepage():
     return render_template("home.html")
 
-@app.route("/squadre")
-def squadre():
-    return render_template("squadre.html")
+@app.route("/calendario")
+def calendario():
+    return render_template("calendario.html")
+
+@app.route("/classifica")
+def classifica():
+    return render_template("classifica.html")
 
 @app.route("/calciatori")
 def calciatori():
