a5bb8fcf78009054c316bc5909a5c42c4101b11a
Zerok00 <lucarellidavide5@gmail.com>
2024-02-09 09:39:05 +0100

    Aggiunte route

--- route.py
+++ route.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def homepage():
     return render_template("home.html")
 
+@app.route("/squadre")
+def squadre():
+    return render_template("squadre.html")
+
+@app.route("/calciatori")
+def calciatori():
+    return render_template("calciatori.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
