14659ec8319decc893eb2a45beefbce4fe2a68b4
Zerok00 <lucarellidavide5@gmail.com>
2024-02-09 11:48:15 +0100

    Creata funzione che calcola la classifica in route

--- route.py
+++ route.py
@@ -1,5 +1,6 @@
 import json
 import mysql.connector
+from funzioni_connessioni import *
 from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
@@ -13,8 +14,42 @@ def calendario():
     return render_template("calendario.html")
 
 @app.route("/classifica")
-def classifica():
-    return render_template("classifica.html")
+def classifica(): #Davide
+    query_squadre = "SELECT * FROM squadra"
+    squadre = execute_query(query_squadre)
+    classifica = []  #conterrà ogni squadra con vittorie, sconfitte e pareggi e verrà ritornato alla fine
+    for elem in squadre:
+        squadra = {"id": elem["id_squadra"],"nome": elem["nome"],"v":0, "s":0, "p":0}
+        classifica.append(squadra)
+
+    query_risultati = "SELECT * FROM risultati"
+    risultati = execute_query(query_risultati)
+
+    for elem in risultati:      #funzione che calcola vittorie, sconfitte e pareggi dalla tabella risultati
+        if elem["gs1"] > elem["gs2"]:
+            for squadra in classifica:
+                if squadra["id"] == elem["id_s1"]:
+                    squadra["v"] += 1
+            for squadra in classifica:
+                if squadra["id"] == elem["id_s2"]:
+                    squadra["s"] += 1
+
+        elif elem["gs1"] == elem["gs2"]:
+            for squadra in classifica:
+                if squadra["id"] == elem["id_s1"]:
+                    squadra["p"] += 1
+            for squadra in classifica:
+                if squadra["id"] == elem["id_s2"]:
+                    squadra["p"] += 1
+        elif elem["gs1"] < elem["gs2"]:
+            for squadra in classifica:
+                if squadra["id"] == elem["id_s1"]:
+                    squadra["s"] += 1
+            for squadra in classifica:
+                if squadra["id"] == elem["id_s2"]:
+                    squadra["v"] += 1
+    # classifica è una lista di dizionari
+    return render_template("classifica.html", classifica=classifica)
 
 @app.route("/calciatori")
 def calciatori():
